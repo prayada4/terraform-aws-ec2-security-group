b4a5069588f369c376fe91aef48c913b58ccc7b3
prayada4 <121497330+prayada4@users.noreply.github.com>
2023-01-09 19:32:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,22 +4,14 @@ resource "aws_security_group" "allow_tls" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-  }
-  
-    ingress {
-    description      = "https from outside"
+    description      = "http traffic from outside"
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  
 
   egress {
     from_port        = 0
