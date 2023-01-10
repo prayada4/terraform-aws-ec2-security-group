c5ea166207474d1a415c9def0f1ff4c657503209
prayada4 <121497330+prayada4@users.noreply.github.com>
2023-01-10 20:29:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = var.vpc_id
 
   ingress {
     description      = "http traffic from outside"
